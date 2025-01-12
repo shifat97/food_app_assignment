146b7684d1abe9cdfc8208e30c7134cfc4b9343b
shifat97 <shifatbinreza2@gmail.com>
2025-01-12 12:21:17 +0600

    add appbar

---
+++ lib/app_bar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class AppBarView extends StatelessWidget {
+  const AppBarView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 30,
+      ),
+      color: Colors.white,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center ,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SvgPicture.asset(
+            'assets/icons/hamburger.svg',
+            width: 25,
+            height: 15,
+          ),
+          Column(
+            children: [
+              Icon(
+                Icons.shopping_basket_outlined,
+                color: Color(0xFFFFA451),
+              ),
+              const SizedBox(height: 5),
+              Text('My Basket', style: TextStyle(
+                fontFamily: 'Poppins'
+              ),)
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
